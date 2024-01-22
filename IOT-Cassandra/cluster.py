7aab063295eeaee44af5f152ef3e479a2120a27b
Emmanuel Jovanovic <cavinomanu@gmail.com>
2024-01-22 16:06:06 +0100

    insert working

--- IOT-Cassandra/cluster.py
+++ IOT-Cassandra/cluster.py
@@ -2,6 +2,7 @@ from cassandra.cluster import Cluster
 import time
 from datetime import datetime
 import random
+from uuid import uuid4
 
 try:
     cluster = Cluster(["127.0.0.1"])
@@ -28,23 +29,25 @@ try:
     print("Keyspace and table exist. Connection is working!")
     cpt = 60
     while cpt != 0:
-        temperature = random.uniform(20.0, 30.0)
+        temperature = random.uniform(0.0, 100.0)
+        sensor_id = uuid4()
         # TODO: Send data to the data ingestion component    print(f"Timestamp: {timestamp}, Temperature: {temperature}")
         cpt -= 1
         session.execute(
             """
-            INSERT INTO temperature_data (temperature)
+            INSERT INTO temperature_data (sensor_id, temperature)
             VALUES (%s, %s)
             """,
-            (temperature),
+            (sensor_id, temperature)
         )
         time.sleep(1)
 
     print("Data sent to the data ingestion component")
 
     rows = session.execute("SELECT * FROM temperature_data")
-    for row in rows:
-        print(row)
+    print(rows)
+    # for row in rows:
+    #     print(row)
 
 except Exception as e:
     print(f"Error: {e}")
