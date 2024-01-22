85de6e51c4ae9e4977e2ad277643ca4908500f63
leo <leo.grandmorcel@ynov.com>
2024-01-22 15:49:16 +0100

    push

--- IOT-Cassandra/cluster.py
+++ IOT-Cassandra/cluster.py
@@ -1,33 +1,55 @@
 from cassandra.cluster import Cluster
+import time
+from datetime import datetime
+import random
 
 try:
-    cluster = Cluster(['127.0.0.1'])
+    cluster = Cluster(["127.0.0.1"])
     session = cluster.connect()
 
-    # Créez le keyspace 'iot_data' s'il n'existe pas
-    session.execute("""
+    session.execute(
+        """
         CREATE KEYSPACE IF NOT EXISTS iot_data 
         WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
-    """)
+    """
+    )
 
-    # Utilisez le keyspace 'iot_data'
-    session.set_keyspace('iot_data')
+    session.set_keyspace("iot_data")
 
-    # Créez la table 'temperature_data' si elle n'existe pas
-    session.execute("""
+    session.execute(
+        """
         CREATE TABLE IF NOT EXISTS temperature_data (
             sensor_id UUID PRIMARY KEY,
             temperature double
         )
-    """)
+    """
+    )
 
     print("Keyspace and table exist. Connection is working!")
+    cpt = 60
+    while cpt != 0:
+        temperature = random.uniform(20.0, 30.0)
+        # TODO: Send data to the data ingestion component    print(f"Timestamp: {timestamp}, Temperature: {temperature}")
+        cpt -= 1
+        session.execute(
+            """
+            INSERT INTO temperature_data (temperature)
+            VALUES (%s, %s)
+            """,
+            (temperature),
+        )
+        time.sleep(1)
+
+    print("Data sent to the data ingestion component")
+
+    rows = session.execute("SELECT * FROM temperature_data")
+    for row in rows:
+        print(row)
 
 except Exception as e:
     print(f"Error: {e}")
 
 finally:
-    # Fermez la session et le cluster de manière plus robuste
     try:
         if session:
             session.shutdown()
